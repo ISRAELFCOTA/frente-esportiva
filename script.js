33a2021debf49506a252fabde3fc6b28b89096d0
ISRAELFCOTA <89460738+ISRAELFCOTA@users.noreply.github.com>
2023-05-10 16:04:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+  reset: true,
+}).reveal(".produtos");
+ScrollReveal({
+  reset: true,
+}).reveal(".historia");
+ScrollReveal({
+  reset: true,
+}).reveal(".masculino");
+ScrollReveal({
+  reset: true,
+}).reveal(".feminino");
+
+
